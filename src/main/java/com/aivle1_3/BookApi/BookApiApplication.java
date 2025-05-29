11595f5311f74af279c8a0f5775514e024aa4ae6
uchan <syt07203@gmail.com>
2025-05-29 15:20:45 +0900

    initialize

---
+++ src/main/java/com/aivle1_3/BookApi/BookApiApplication.java
@@ -0,0 +1,13 @@
+package com.aivle1_3.BookApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aivle1_3/BookApi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle1_3.BookApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
