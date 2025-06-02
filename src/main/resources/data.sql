1a79d4dfb5fc44d4f67c4e17d8c6caccba4bbd17
uchan <syt07203@gmail.com>
2025-06-02 14:49:05 +0900

    userid 3부터 시작하도록 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+ALTER TABLE users ALTER COLUMN id RESTART WITH 3;
 INSERT INTO users (id, userid, password, nickname) VALUES (1 ,'user1', 'pass1', '홍길동');
 INSERT INTO users (id, userid, password, nickname) VALUES (2 ,'user2', 'pass2', '임꺽정');
 
