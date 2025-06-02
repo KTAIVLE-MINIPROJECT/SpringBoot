ec1950b78a0a5fb145bc742d81c61b4427b6e9b7
uchan <syt07203@gmail.com>
2025-06-02 10:00:19 +0900

    WebCOnfig

---
+++ src/main/java/com/aivle1_3/BookApi/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.aivle1_3.BookApi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")  // 프론트 포트 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
